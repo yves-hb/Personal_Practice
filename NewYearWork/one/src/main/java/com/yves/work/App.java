153ca4ab762224088a5e64bf99363ad919ed786a
hb_yves <yves_hb@163.com>
2023-01-16 10:44:18 +0800

    create a <newYearWork> project

---
+++ NewYearWork/one/src/main/java/com/yves/work/App.java
@@ -0,0 +1,15 @@
+package com.yves.work;
+
+import java.util.HashMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        HashMap<String,String> map = new HashMap<String,String>();
+    }
+}
